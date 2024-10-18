2fb2a3cd16d8385a0bd63af4e00f31cc4a3a5777
Saeid.Tadrisi <saeid.tadrisi@gmail.com>
2024-10-18 11:43:11 +0200

    snakes and moves created.

---
+++ snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.snakes_init()
+        self.head = self.segments[0]
+
+    def snakes_init(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_index in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_index - 1].xcor()
+            new_y = self.segments[seg_index - 1].ycor()
+            self.segments[seg_index].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(0)
